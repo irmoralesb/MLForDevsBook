6c9eefc7a9a9b8fc29d0605d7c751a9ddb34834f
irmorales <irmoralesb@hotmail.com>
2018-12-24 14:28:56 -0600

    Modified 02 dataExploation_and_linearRegression file, completed Added 03 polynomialRegression file (empty)

--- Source/Chapter4/02 dataExploration_and_linearRegression.py
+++ Source/Chapter4/02 dataExploration_and_linearRegression.py
@@ -28,3 +28,45 @@ def correlation(X, Y):
 
 print("My Correlation: {}".format(correlation([1, 1, 4, 3], [1, 0, 2, 2])))
 print("Numpy corrcoef: {}".format(np.corrcoef([1, 1, 4, 3], [1, 0, 2, 2])))
+
+# Start seeing a general view of the data to try to determine what is the best approach
+
+sns.pairplot(iris2, height=3.0)
+plt.show()
+# Based on the results, we chose
+X = iris2['petal_width']
+Y = iris2['petal_length']
+
+plt.scatter(X, Y)
+#plt.show()
+
+
+# Creating the prediction function
+def predict(alpha, beta, x_i):
+    return beta * x_i + alpha
+
+
+# Defining the error function
+def error(alpha, beta, x_i, y_i):  # L1
+    return y_i - predict(alpha, beta, x_i)
+
+
+def sum_sq_e(alpha, beta, x, y):  # L2
+    return sum(error(alpha, beta, x_i, y_i) ** 2 for x_i, y_i in zip(x, y))
+
+
+# Now we define a function implementing the correlation method to find the parameters for our regression
+def correlation_fit(x, y):
+    beta = correlation(x, y) * np.std(y, ddof=1) / np.std(x, ddof=1)
+    alpha = np.mean(y) - beta * np.mean(x)
+    return alpha, beta
+
+
+alpha, beta = correlation_fit(X, Y)
+print("Alpha: {}".format(alpha))
+print("Beta: {}".format(beta))
+
+plt.scatter(X, Y)
+xr = np.arange(0, 3.5)
+plt.plot(xr, (xr * beta) + alpha)
+plt.show()
