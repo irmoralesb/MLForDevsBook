3ca99868a6b00d73a4af45e5c1d7f9dc59a94fee
irmorales <irmoralesb@hotmail.com>
2019-01-04 09:25:52 -0600

    Chapter 6 (Completed) * Updating ConvolutionalNetworkQuiver

--- Source/Chapter6/06 transferLearning.py
+++ Source/Chapter6/06 transferLearning.py
@@ -37,10 +37,10 @@ predictions = Dense(2, activation='softmax')(x)
 model_final = Model(input=model.input, output=predictions)
 
 # compile the model
-model_final.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.0001, momntum=0.9),
+model_final.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.0001, momentum=0.9),
                     metrics=["accuracy"])
 
-# Initiate the train and test generators with data Augumentation
+# Initiate the train and test generators with data Augmentation
 train_datagen = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True, fill_mode='nearest', zoom_range=0.3,
                                    width_shift_range=0.3, height_shift_range=0.3, rotation_range=30)
 
@@ -49,15 +49,10 @@ test_datagen = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True, fill_m
 
 # Now we will generate the new augmented data
 train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
-    class_mode="categorical")
+    train_data_dir, target_size=(img_height, img_width), batch_size=batch_size, class_mode="categorical")
 
-validation_generator = test_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=(img_height, img_width),
-    class_mode="categorical")
+validation_generator = test_datagen.flow_from_directory(validation_data_dir, target_size=(img_height, img_width),
+                                                        class_mode="categorical")
 
 # Save the model according to the conditions
 checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False,
@@ -66,10 +61,15 @@ early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mo
 
 # It's time to fit the new final layers for the model:
 
-model_final.fit_generator(
-    train_generator,
-    samples_per_epoch=nb_train_samples,
-    nb_epoch=epochs,
-    validation_data=validation_generator,
-    nb_val_samples=nb_validation_samples,
-    callbacks=[checkpoint, early])
+model_final.fit_generator(train_generator, samples_per_epoch=nb_train_samples, nb_epoch=epochs,
+                          validation_data=validation_generator, nb_val_samples=nb_validation_samples,
+                          callbacks=[checkpoint, early])
+
+im = cv2.resize(cv2.imread('test/gaff2.jpg', (img_width, img_height)))
+im = np.expand_dims(im, axis=0).astype(np.float)
+im = preprocess_input(im)
+
+out = model_final.predict(im)
+
+print(out)
+print(np.argmax(out))
