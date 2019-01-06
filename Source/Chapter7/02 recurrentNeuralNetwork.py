4e990d720ef5888525d09d2e27e37a4db21a75db
irmorales <irmoralesb@hotmail.com>
2019-01-05 18:44:32 -0600

    Chapter 7 Modified and running, need to check last chart

--- Source/Chapter7/02 recurrentNeuralNetwork.py
+++ Source/Chapter7/02 recurrentNeuralNetwork.py
@@ -1,62 +1,63 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from matplotlib.pyplot import plt
+import matplotlib.pyplot as plt
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
 from keras.layers import Dropout
 
-df = pd.read_csv('date/elec_load.csv', error_bad_lines=False)
+df = pd.read_csv('data/elec_load.csv', error_bad_lines=False)
 plt.subplot()
 plot_test, = plt.plot(df.values[:1500], label='Load')
 plt.legend(handles=[plot_test])
-
+plt.show()
 print(df.describe())
 array = (df.values - 145.33) / 338.21  # minus the mean and divided between the max value
 plt.subplot()
 plot_test, = plt.plot(array[:1500], label='Normalized Load')
 plt.legend(handles=[plot_test])
+plt.show()
 
 listX = []
-listY = []
-x = {}
+listy = []
+X = {}
 y = {}
 
 for i in range(0, len(array) - 6):
     listX.append(array[i:i + 5].reshape([5, 1]))
-    listY.append(array[i + 6])
+    listy.append(array[i + 6])
 
 arrayX = np.array(listX)
-arrayY = np.array(listY)
+arrayY = np.array(listy)
 
-x['train'] = arrayX[0:13000]
-x['test'] = arrayY[13000:14000]
+X['train'] = arrayX[0:13000]
+X['test'] = arrayX[13000:14000]
 y['train'] = arrayY[0:13000]
-y['test'] = arrayY[13000, 14000]
+y['test'] = arrayY[13000:14000]
 
 # Build the model
 model = Sequential()
 model.add(LSTM(units=50, input_shape=(None, 1), return_sequences=True))
 model.add(Dropout(0.2))
-
 model.add(LSTM(units=200, input_shape=(None, 100), return_sequences=False))
 model.add(Dropout(0.2))
-
 model.add(Dense(units=1))
 model.add(Activation("linear"))
 
 model.compile(loss="mse", optimizer="rmsprop")
 
 # Fit the model to the data
-model.fit(x['train'], y['train'], batch_size=512, epochs=10, validation_split=0.08)
+model.fit(X['train'], y['train'], batch_size=512, epochs=10, validation_split=0.08)
 
 # Rescale the test dataset and predicted data
-test_results = model.predict(x[' test'])
+test_results = model.predict(X['test'])
 test_results = test_results * 338.21 + 145.33
-y[' test'] = y[' test'] * 338.21 + 145.33
-plt.figure(figsize=(10, 15))
-plot_predicted, = plt.plot(test_results, label=' predicted')
-plot_test, = plt.plot(y[' test'], label=' test')
+y['test'] = y['test'] * 338.21 + 145.33
+
+plt.figure(figsize=(40, 30))
+plot_predicted, = plt.plot(test_results, label='predicted')
+plot_test, = plt.plot(y['test'], label='test')
 plt.legend(handles=[plot_predicted, plot_test])
+plt.show()
